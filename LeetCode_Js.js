5f8494683fe638d2ed9600c7b5eeb69734a34eec
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-07-14 00:03:33 +0530

    DAY 5

--- LeetCode_Js.js
+++ LeetCode_Js.js
@@ -444,3 +444,109 @@ var compose = function(functions) {
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
  */
+
+
+// 2703. Return Length of Arguments Passed
+
+// Write a function argumentsLength that returns the count of arguments passed to it.
+ 
+// Example 1:
+
+// Input: args = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+
+// One value was passed to the function so it should return 1.
+// Example 2:
+
+// Input: args = [{}, null, "3"]
+// Output: 3
+// Explanation: 
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
+ 
+// Constraints:
+
+// args is a valid JSON array
+// 0 <= args.length <= 100
+
+// Sol_9}
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    // return args.length ---------->> there is another and better method then this
+    let count = 0
+    for(let i =0; i < args.length; i++){
+        count += 1
+    }
+    return count
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+
+// 2666. Allow One Function Call
+
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+// Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+ 
+
+// Constraints:
+
+// calls is a valid JSON array
+// 1 <= calls.length <= 10
+// 1 <= calls[i].length <= 100
+// 2 <= JSON.stringify(calls).length <= 1000
+
+// Sol_10}
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false
+    return function(...args){
+        if(called){
+            return undefined
+        }
+        called = true
+        return fn(...args)
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
