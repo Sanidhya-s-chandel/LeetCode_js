7b95dd6c755bc3217a56bb4a938973b7625889ce
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-07-15 00:14:29 +0530

    DAY 6

--- LeetCode_Js.js
+++ LeetCode_Js.js
@@ -550,3 +550,145 @@ var once = function(fn) {
  * onceFn(1,2,3); // 6
  * onceFn(2,3,6); // returns undefined without calling fn
  */
+
+
+// 2623. Memoize
+
+// Given a function fn, return a memoized version of that function.
+
+// A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+// You can assume there are 3 possible input functions: sum, fib, and factorial.
+
+// sum accepts two integers a and b and returns a + b. Assume that if a value has already been cached for the arguments (b, a) where a != b, it cannot be used for the arguments (a, b). For example, if the arguments are (3, 2) and (2, 3), two separate calls should be made.
+// fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2) otherwise.
+// factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
+ 
+
+// Example 1:
+
+// Input:
+// fnName = "sum"
+// actions = ["call","call","getCallCount","call","getCallCount"]
+// values = [[2,2],[2,2],[],[1,2],[]]
+// Output: [4,4,1,3,2]
+// Explanation:
+// const sum = (a, b) => a + b;
+// const memoizedSum = memoize(sum);
+// memoizedSum(2, 2); // "call" - returns 4. sum() was called as (2, 2) was not seen before.
+// memoizedSum(2, 2); // "call" - returns 4. However sum() was not called because the same inputs were seen before.
+// // "getCallCount" - total call count: 1
+// memoizedSum(1, 2); // "call" - returns 3. sum() was called as (1, 2) was not seen before.
+// // "getCallCount" - total call count: 2
+// Example 2:
+
+// Input:
+// fnName = "factorial"
+// actions = ["call","call","call","getCallCount","call","getCallCount"]
+// values = [[2],[3],[2],[],[3],[]]
+// Output: [2,6,2,2,6,2]
+// Explanation:
+// const factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
+// const memoFactorial = memoize(factorial);
+// memoFactorial(2); // "call" - returns 2.
+// memoFactorial(3); // "call" - returns 6.
+// memoFactorial(2); // "call" - returns 2. However factorial was not called because 2 was seen before.
+// // "getCallCount" - total call count: 2
+// memoFactorial(3); // "call" - returns 6. However factorial was not called because 3 was seen before.
+// // "getCallCount" - total call count: 2
+// Example 3:
+
+// Input:
+// fnName = "fib"
+// actions = ["call","getCallCount"]
+// values = [[5],[]]
+// Output: [8,1]
+// Explanation:
+// fib(5) = 8 // "call"
+// // "getCallCount" - total call count: 1
+ 
+
+// Constraints:
+
+// 0 <= a, b <= 105
+// 1 <= n <= 10
+// 0 <= actions.length <= 105
+// actions.length === values.length
+// actions[i] is one of "call" and "getCallCount"
+// fnName is one of "sum", "factorial" and "fib"
+
+// Sol_11}
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache = {}
+    return function(...args) {
+        let n = JSON.stringify(args)
+        if (n in cache){
+            return cache[n]
+        }else{
+            let result = fn.apply(this , args)
+            cache[n] = result
+            return result
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
+
+
+//  2723. Add Two Promises
+
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+ 
+
+// Example 1:
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+// Output: 7
+// Explanation: The two input promises resolve with the values of 2 and 5 respectively. The returned promise should resolve with a value of 2 + 5 = 7. The time the returned promise resolves is not judged for this problem.
+// Example 2:
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30))
+// Output: -2
+// Explanation: The two input promises resolve with the values of 10 and -12 respectively. The returned promise should resolve with a value of 10 + -12 = -2.
+ 
+
+// Constraints:
+
+// promise1 and promise2 are promises that resolve with a number
+
+// Sol_12}
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [val1 , val2] = await Promise.all([promise1, promise2])
+
+    return val1 + val2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
