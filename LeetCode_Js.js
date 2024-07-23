7dcad198f5a751b7604b67eaca2d0d4a1f106a57
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-07-23 22:20:06 +0530

    DAY 11

--- LeetCode_Js.js
+++ LeetCode_Js.js
@@ -1371,3 +1371,99 @@ var isEmpty = function(obj) {
         return arr.length === 0
     }
 };
+
+
+// 2677. Chunk Array
+
+// Given an array arr and a chunk size size, return a chunked array.
+
+// A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
+
+// You may assume the array is the output of JSON.parse. In other words, it is valid JSON.
+
+// Please solve it without using lodash's _.chunk function.
+
+// Example 1:
+
+// Input: arr = [1,2,3,4,5], size = 1
+// Output: [[1],[2],[3],[4],[5]]
+// Explanation: The arr has been split into subarrays each with 1 element.
+// Example 2:
+
+// Input: arr = [1,9,6,3,2], size = 3
+// Output: [[1,9,6],[3,2]]
+// Explanation: The arr has been split into subarrays with 3 elements. However, only two elements are left for the 2nd subarray.
+// Example 3:
+
+// Input: arr = [8,5,3,2,6], size = 6
+// Output: [[8,5,3,2,6]]
+// Explanation: Size is greater than arr.length thus all elements are in the first subarray.
+// Example 4:
+
+// Input: arr = [], size = 1
+// Output: []
+// Explanation: There are no elements to be chunked so an empty array is returned.
+ 
+
+// Constraints:
+
+// arr is a valid JSON array
+// 2 <= JSON.stringify(arr).length <= 105
+// 1 <= size <= arr.length + 1
+
+// Sol_21}
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let result = []
+
+    for(let i = 0; i < arr.length; i += size){
+        result.push(arr.slice(i,i+ size))
+    }
+
+    return result
+};
+
+// 2619. Array Prototype Last
+
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// You may assume the array is the output of JSON.parse.
+
+// Example 1:
+
+// Input: nums = [null, {}, 3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+// Example 2:
+
+// Input: nums = []
+// Output: -1
+// Explanation: Because there are no elements, return -1.
+ 
+
+// Constraints:
+
+// arr is a valid JSON array
+// 0 <= arr.length <= 1000
+
+// Sol_22}
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length == 0){
+        return -1;
+    }
+    return this[this.length-1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
