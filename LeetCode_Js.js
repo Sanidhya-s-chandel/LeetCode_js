4e4af4766e416b4d4546699d7a383543adc9ac39
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-29 22:23:40 +0530

    Completed 30-Js

--- LeetCode_Js.js
+++ LeetCode_Js.js
@@ -1977,15 +1977,12 @@ class EventEmitter {
  */
 
 // 2695. Array Wrapper
-// Solved
-// Easy
-// Companies
+
 // Create a class ArrayWrapper that accepts an array of integers in its constructor. This class should have two features:
 
 // When two instances of this class are added together with the + operator, the resulting value is the sum of all the elements in both arrays.
 // When the String() function is called on the instance, it will return a comma separated string surrounded by brackets. For example, [1,2,3].
  
-
 // Example 1:
 
 // Input: nums = [[1,2],[3,4]], operation = "Add"
@@ -2010,7 +2007,6 @@ class EventEmitter {
 // const obj2 = new ArrayWrapper([]);
 // obj1 + obj2; // 0
  
-
 // Constraints:
 
 // 0 <= nums.length <= 1000
@@ -2048,3 +2044,122 @@ ArrayWrapper.prototype.toString = function() {
  * String(obj1); // "[1,2]"
  * String(obj2); // "[3,4]"
  */
+
+// 2726. Calculator with Method Chaining
+
+// Design a Calculator class. The class should provide the mathematical operations of addition, subtraction, multiplication, division, and exponentiation. It should also allow consecutive operations to be performed using method chaining. The Calculator class constructor should accept a number which serves as the initial value of result.
+
+// Your Calculator class should have the following methods:
+
+// add - This method adds the given number value to the result and returns the updated Calculator.
+// subtract - This method subtracts the given number value from the result and returns the updated Calculator.
+// multiply - This method multiplies the result  by the given number value and returns the updated Calculator.
+// divide - This method divides the result by the given number value and returns the updated Calculator. If the passed value is 0, an error "Division by zero is not allowed" should be thrown.
+// power - This method raises the result to the power of the given number value and returns the updated Calculator.
+// getResult - This method returns the result.
+// Solutions within 10-5 of the actual result are considered correct.
+
+// Example 1:
+
+// Input: 
+// actions = ["Calculator", "add", "subtract", "getResult"], 
+// values = [10, 5, 7]
+// Output: 8
+// Explanation: 
+// new Calculator(10).add(5).subtract(7).getResult() // 10 + 5 - 7 = 8
+// Example 2:
+
+// Input: 
+// actions = ["Calculator", "multiply", "power", "getResult"], 
+// values = [2, 5, 2]
+// Output: 100
+// Explanation: 
+// new Calculator(2).multiply(5).power(2).getResult() // (2 * 5) ^ 2 = 100
+// Example 3:
+
+// Input: 
+// actions = ["Calculator", "divide", "getResult"], 
+// values = [20, 0]
+// Output: "Division by zero is not allowed"
+// Explanation: 
+// new Calculator(20).divide(0).getResult() // 20 / 0 
+
+// The error should be thrown because we cannot divide by zero.
+ 
+// Constraints:
+
+// actions is a valid JSON array of strings
+// values is a valid JSON array of numbers
+// 2 <= actions.length <= 2 * 104
+// 1 <= values.length <= 2 * 104 - 1
+// actions[i] is one of "Calculator", "add", "subtract", "multiply", "divide", "power", and "getResult"
+// First action is always "Calculator"
+// Last action is always "getResult"
+
+// Sol_30}
+
+class Calculator {
+    
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.result = value
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value){
+        this.result += value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value){
+        this.result -= value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        this.result *= value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if(value === 0){
+            throw new Error("Division by zero is not allowed")
+        }
+
+        this.result /= value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.result **= value
+        return this
+    }
+    
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.result
+    }
+}
