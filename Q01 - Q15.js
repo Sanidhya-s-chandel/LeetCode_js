8fccb72809ec70e9307e7ca491ed84f4ab19d2f0
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-21 21:53:44 +0530

    Q 10

--- Q01 - Q15.js
+++ Q01 - Q15.js
@@ -531,3 +531,87 @@ var isPalindrome = function (x) {
     }
     return true
 };
+
+// 10.) Regular Expression Matching
+// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+// '.' Matches any single character.​​​​
+// '*' Matches zero or more of the preceding element.
+// The matching should cover the entire input string (not partial).
+
+// Example 1:
+
+// Input: s = "aa", p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+// Example 2:
+
+// Input: s = "aa", p = "a*"
+// Output: true
+// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+// Example 3:
+
+// Input: s = "ab", p = ".*"
+// Output: true
+// Explanation: ".*" means "zero or more (*) of any character (.)".
+ 
+// Constraints:
+
+// 1 <= s.length <= 20
+// 1 <= p.length <= 20
+// s contains only lowercase English letters.
+// p contains only lowercase English letters, '.', and '*'.
+// It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
+// Sol_10}
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+const match = (ch, pt) => {
+    if(pt === "." || ch === pt) return true;
+    return false;
+}
+
+var isMatch = function(s, p) {
+
+    let i = 0, j = 0;;
+    while(i < s.length && j < p.length) {
+        let ch = s[i];
+        const pt = p[j];
+        if(j < p.length - 1 && p[j+1] === "*") {
+            j += 2;
+            let newP;
+            if (j < p.length) {
+                newP = p.substring(j);
+                let newS = s.substring(i);
+                if(isMatch(newS, newP)) return true;
+            }
+            while(match(ch, pt)) {
+                ++i;
+
+                if (j < p.length) {
+                    let newS = s.substring(i);
+                    if(isMatch(newS, newP)) return true;
+                }
+
+                if(i === s.length) break;
+                ch = s[i];
+            }
+        } else {
+            if(!match(ch, pt)) return false;
+            ++i;
+            ++j;
+        }
+    }
+
+    while(j < p.length - 1 && p[j+1] === "*") {
+        j += 2;
+    }
+
+    let res = j === p.length && i === s.length;
+
+    return res;
+};
