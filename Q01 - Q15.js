21b90bff19298cc247bf6556e397e76dfeadc7d1
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-21 21:52:26 +0530

    Q 09

--- Q01 - Q15.js
+++ Q01 - Q15.js
@@ -484,3 +484,50 @@ var myAtoi = function (s) {
 
     return 0;
 };
+
+// 9.) Palindrome Number
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+// Follow up: Could you solve it without converting the integer to a string?
+
+// Sol_09}
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    x = x.toString()
+    let i = 0
+    let j = x.length - 1
+    while (i < j) {
+        if (x[i] != x[j]) {
+            return false
+        }
+        i++ 
+        j--
+    }
+    return true
+};
