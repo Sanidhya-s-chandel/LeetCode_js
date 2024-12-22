aceaf5e8470777ee783b22b8a83685425f1f1e99
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-22 22:58:12 +0530

    Q 11

--- Q01 - Q15.js
+++ Q01 - Q15.js
@@ -615,3 +615,71 @@ var isMatch = function(s, p) {
 
     return res;
 };
+
+// 11.) Container With Most Water
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container.
+
+// Example 1:
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+// Example 2:
+
+// Input: height = [1,1]
+// Output: 1
+
+// Constraints:
+
+// n == height.length
+// 2 <= n <= 105
+// 0 <= height[i] <= 104
+
+// sol_11}
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = height[0];
+    let length = height.length;
+    let right = height[length];  
+    let leftIndex = 0;
+    let rightIndex = length -1;
+    let max = 0;
+    let containerMax = 0;
+    let index =0;
+    while((leftIndex < rightIndex))
+    {
+      if(height[leftIndex] > height[rightIndex]) // meaning right value to be considered
+      {
+          index = rightIndex - leftIndex;
+          if(containerMax < (height[rightIndex]*index))
+          {
+              
+              console.log("Index = ",index)
+              containerMax = height[rightIndex]*index}
+      }
+      else
+      {
+          index = rightIndex - leftIndex;
+          if(containerMax < (height[leftIndex]*index))
+          {
+              console.log("Index = ",index)
+              containerMax = height[leftIndex]*index}
+      }
+     
+      if(height[leftIndex] > height[rightIndex])
+      rightIndex--;
+      else
+      leftIndex++;
+    }
+    return containerMax;
+  };
