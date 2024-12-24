14041e1e1f0ab224dbe13c574fe18a93063ffc11
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-24 21:24:14 +0530

    Q 14

--- Q01 - Q15.js
+++ Q01 - Q15.js
@@ -855,3 +855,52 @@ var romanToInt = function(s) {
 
     return result
 };
+
+// 14.) Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+ 
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters.
+
+// Sol_14}
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let output = "";
+    let index = 0;
+    let mn = strs.reduce((min, current) => {
+            return current.length < min.length ? current : min;
+            }, strs[0]);
+    for(index=0; index < mn.length; index++){
+        let temp = [];
+        for(i=0; i < strs.length; i++){
+            temp.push(strs[i][index]);
+        }
+        let size_ = new Set(temp).size;
+        if(size_== 1){
+            output += temp[0];
+        }else{
+            break;
+        }
+    }
+
+    return output;
+};
