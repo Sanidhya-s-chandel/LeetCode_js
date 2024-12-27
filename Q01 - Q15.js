3fee25c97b3389b42adb445028bcd96483ebc8a6
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-27 20:57:08 +0530

    Q 18

--- Q01 - Q15.js
+++ Q01 - Q15.js
@@ -1092,3 +1092,73 @@ var letterCombinations = function(digits) {
     backtrack(0, "");  
     return res;    
 };
+
+// 18.) 4Sum
+// Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+// 0 <= a, b, c, d < n
+// a, b, c, and d are distinct.
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+// You may return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,0,-1,0,-2,2], target = 0
+// Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// Example 2:
+
+// Input: nums = [2,2,2,2,2], target = 8
+// Output: [[2,2,2,2]]
+ 
+// Constraints:
+
+// 1 <= nums.length <= 200
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+
+// Sol_18}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    let mySet = new Set();
+nums.sort((a, b) => a - b);
+let result = [];
+for (let i = 0; i < nums.length; i++) {
+  for (let j = i + 1; j < nums.length; j++) {
+    k = j + 1;
+    let l = nums.length - 1;
+    while (k < l) {
+      let sum = nums[i] + nums[j] + nums[k] + nums[l];
+      if (sum < target) {
+        k++;
+        while (nums[k] == nums[k - 1]) {
+          k++;
+        }
+      } else if (sum > target) {
+        l--;
+        while (nums[l] == nums[l + 1]) {
+          l--;
+        }
+      } else if (sum == target) {
+        let temp = [nums[i], nums[j], nums[k], nums[l]];
+        temp.sort((a, b) => a - b);
+        mySet.add(JSON.stringify(temp));
+        k++;
+        while (nums[k] == nums[k - 1]) {
+          k++;
+        }
+        l--;
+        while (nums[l] == nums[l + 1]) {
+          l--;
+        }
+      }
+    }
+  }
+}
+mySet.forEach((v) => result.push(JSON.parse(v)));
+return result;
+};
