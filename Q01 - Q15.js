aa61a70d38d000a51d6bf3ee3837ccc6e3a678dc
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-28 21:52:49 +0530

    Q 19

--- Q01 - Q15.js
+++ Q01 - Q15.js
@@ -1162,3 +1162,69 @@ for (let i = 0; i < nums.length; i++) {
 mySet.forEach((v) => result.push(JSON.parse(v)));
 return result;
 };
+
+// 19.) Remove Nth Node From End of List
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+// Example 1:
+
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
+
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
+
+// Input: head = [1,2], n = 1
+// Output: [1]
+ 
+// Constraints:
+
+// The number of nodes in the list is sz.
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
+ 
+// Follow up: Could you do this in one pass?
+
+// Sol_19}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    class Node {
+        constructor(data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    let dummy = new Node(0);
+    dummy.next = head;
+    let slow = dummy;
+    let fast = dummy;
+
+    for(let i=0; i<=n; i++) {
+        fast = fast.next;
+    }
+
+    while(fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    slow.next = slow.next.next;
+    return dummy.next;
+
+};
