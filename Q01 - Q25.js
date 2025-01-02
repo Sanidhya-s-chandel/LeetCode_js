2e609755336c4aec131a1347a9d1e93209c26992
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2025-01-02 23:02:10 +0530

    Q 24

--- Q01 - Q25.js
+++ Q01 - Q25.js
@@ -1522,3 +1522,54 @@ function mergeLists(l1, l2) {
     node.next = l1 ? l1 : l2;
     return ans.next;
 }
+
+// 24.) Swap Nodes in Pairs
+// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+// Example 1:
+
+// Input: head = [1,2,3,4]
+// Output: [2,1,4,3]
+
+// Explanation:
+
+// Example 2:
+
+// Input: head = []
+// Output: []
+
+// Example 3:
+
+// Input: head = [1]
+// Output: [1]
+
+// Example 4:
+
+// Input: head = [1,2,3]
+// Output: [2,1,3]
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 100].
+// 0 <= Node.val <= 100
+
+// Sol_24}
+
+var swapPairs = function(head) {
+    let dummy = new ListNode(0, head);
+    let prev = dummy, cur = head;
+
+    while (cur && cur.next) {
+        let npn = cur.next.next;
+        let second = cur.next;
+
+        second.next = cur;
+        cur.next = npn;
+        prev.next = second;
+
+        prev = cur;
+        cur = npn;
+    }
+
+    return dummy.next;    
+};
