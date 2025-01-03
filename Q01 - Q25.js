ea04748c75784d10697cacf3e5791ecf9a0a5b93
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2025-01-03 23:11:24 +0530

    Q 25

--- Q01 - Q25.js
+++ Q01 - Q25.js
@@ -1573,3 +1573,75 @@ var swapPairs = function(head) {
 
     return dummy.next;    
 };
+
+// 25.) Reverse Nodes in k-Group
+// Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+// k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+// You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+// Example 1:
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [2,1,4,3,5]
+
+// Example 2:
+// Input: head = [1,2,3,4,5], k = 3
+// Output: [3,2,1,4,5]
+ 
+// Constraints:
+
+// The number of nodes in the list is n.
+// 1 <= k <= n <= 5000
+// 0 <= Node.val <= 1000
+ 
+// Follow-up: Can you solve the problem in O(1) extra memory space?
+
+// Sol_25}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+
+let kthnode = (node , k)=>{
+    let t = node
+    while( t && k>1){
+     t = t.next
+     k--
+    } 
+    return t
+ }
+ 
+ var reverseKGroup = function(head, k) {
+     let dummy2 = new ListNode(0,head)
+     let node = dummy2
+ 
+     while(true){
+         let prev = node.next
+         let kth = kthnode(prev , k)
+         if(!kth){
+           break 
+         }
+         let nextgroup = kth.next
+         let curr = nextgroup
+         let h = prev
+         for(let i= 0;i<k;i++){
+            let temp = h.next
+            h.next = curr
+            curr = h
+            h = temp
+         }
+         node.next = curr
+         prev.next = nextgroup
+         node = prev
+     }
+     return dummy2.next
+ 
+};
